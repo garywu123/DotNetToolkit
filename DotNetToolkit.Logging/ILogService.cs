7dffb5bf51aaeb46ef53dcfd9eee7d367f58a7d4
Gary Wu <Gary.Wu@crown.com>
2025-07-05 15:47:06 -0400

    Add project files.

---
+++ DotNetToolkit.Logging/ILogService.cs
@@ -0,0 +1,25 @@
+﻿#region License
+
+// author:         garyw
+// created:        15:07
+// description:
+
+#endregion
+
+namespace DotNetToolkit.Logging;
+
+public interface ILogService
+{
+    void LogVerbose(string message);
+
+    void LogDebug(string message);
+
+    void LogInformation(string message);
+
+    void LogWarning(string message);
+
+    void LogError(string message);
+
+    void LogCritical(string message);
+
+}
